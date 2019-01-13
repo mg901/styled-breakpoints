efdfbacd38c47cdda177ae243940c8a75b4132bf
mg901 <mgreen097@gmail.com>
2019-01-13 08:28:15 +0300

    refactor: rename constants

--- src/constants.js
+++ src/constants.js
@@ -1,14 +1,14 @@
 // @flow
 
-import type { BreakpointsMap, DefaultUserTheme } from './models';
+import { type BreakpointsMap, ThemeWithBreakpoints } from './models';
 
-export const DEFAULT_BREAKS: BreakpointsMap = {
+export const defaultBreaks: BreakpointsMap = {
   phone: '576px',
   tablet: '768px',
   desktop: '992px',
   lgDesktop: '1200px',
 };
 
-export const THEME_WITH_DEFAULT_BREAKS: DefaultUserTheme = {
-  theme: { breakpoints: DEFAULT_BREAKS },
+export const themeWithDefaultBreaks: ThemeWithBreakpoints = {
+  theme: { breakpoints: defaultBreaks },
 };
