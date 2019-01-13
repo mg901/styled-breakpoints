87045e48b0738abe60b9773c0e7ba4ae9fd1ea55
mg901 <mgreen097@gmail.com>
2019-01-13 08:28:47 +0300

    test: rename constants

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,9 +1,9 @@
 import { createAbove, createBelow, createBetween, createOnly } from '.';
-import { THEME_WITH_DEFAULT_BREAKS } from './constants';
+import { themeWithDefaultBreaks } from './constants';
 
 describe('createAbove', () => {
   it('should return string with min breakpoint value and media query', () => {
-    expect(createAbove('tablet')(THEME_WITH_DEFAULT_BREAKS)).toEqual(
+    expect(createAbove('tablet')(themeWithDefaultBreaks)).toEqual(
       '@media (min-width: 48em)',
     );
   });
@@ -11,7 +11,7 @@ describe('createAbove', () => {
 
 describe('createBelow', () => {
   it('should return string with max breakpoint value and media query', () => {
-    expect(createBelow('tablet')(THEME_WITH_DEFAULT_BREAKS)).toEqual(
+    expect(createBelow('tablet')(themeWithDefaultBreaks)).toEqual(
       '@media (max-width: 61.99875em)',
     );
   });
@@ -19,15 +19,15 @@ describe('createBelow', () => {
 
 describe('createBetween', () => {
   it('should returns a string containing the value of the minimum and maximum breakpoints and media query', () => {
-    expect(
-      createBetween('tablet', 'desktop')(THEME_WITH_DEFAULT_BREAKS),
-    ).toEqual('@media (min-width: 48em) and (max-width: 74.99875em)');
+    expect(createBetween('tablet', 'desktop')(themeWithDefaultBreaks)).toEqual(
+      '@media (min-width: 48em) and (max-width: 74.99875em)',
+    );
   });
 });
 
 describe('createOnly', () => {
   it('should returns a string containing the minimum and maximum values of the current breakpoint and media query', () => {
-    expect(createOnly('tablet')(THEME_WITH_DEFAULT_BREAKS)).toEqual(
+    expect(createOnly('tablet')(themeWithDefaultBreaks)).toEqual(
       '@media (min-width: 48em) and (max-width: 61.99875em)',
     );
   });
