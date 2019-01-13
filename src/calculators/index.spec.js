eafda0a5532111081e88ae895c685110b3743bcc
mg901 <mgreen097@gmail.com>
2019-01-13 08:29:45 +0300

    test(calculators):  add tests for custom theme

--- src/calculators/index.spec.js
+++ src/calculators/index.spec.js
@@ -1,20 +1,23 @@
 import { calcMinWidthInPx, calcMaxWidthInPx } from '.';
-import { THEME_WITH_DEFAULT_BREAKS } from '../constants';
+import { customTheme } from '../mocks';
 
 describe('calculators', () => {
   describe('calcMinWidthInPx', () => {
-    it('calculate min with in pixels', () => {
-      expect(calcMinWidthInPx(THEME_WITH_DEFAULT_BREAKS, 'tablet')).toEqual(
-        '48em',
-      );
+    it('calculate min with in pixels from defualt theme', () => {
+      expect(calcMinWidthInPx(undefined, 'tablet')).toEqual('48em');
+    });
+
+    it('calculate min with in pixels from defualt theme', () => {
+      expect(calcMinWidthInPx(customTheme, 'sm')).toEqual('48em');
     });
   });
 
   describe('calcMaxWidthInPx', () => {
-    it('calculate max with in pixels', () => {
-      expect(calcMaxWidthInPx(THEME_WITH_DEFAULT_BREAKS, 'tablet')).toEqual(
-        '61.99875em',
-      );
+    it('calculate max with in pixels from defualt theme', () => {
+      expect(calcMaxWidthInPx(undefined, 'tablet')).toEqual('61.99875em');
+    });
+    it('calculate max with in pixels from defualt theme', () => {
+      expect(calcMaxWidthInPx(customTheme, 'sm')).toEqual('61.99875em');
     });
   });
 });
