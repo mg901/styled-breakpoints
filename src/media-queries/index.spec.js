2d3cc2e9bf0b968b8b491901014af1f0109dd712
mg901 <mgreen097@gmail.com>
2019-01-13 08:28:47 +0300

    test: rename constants

--- src/media-queries/index.spec.js
+++ src/media-queries/index.spec.js
@@ -4,13 +4,13 @@ import {
   eitherGetNextBreakVal,
 } from '.';
 import { errorReport } from '../helpers';
-import { DEFAULT_BREAKS } from '../constants';
+import { defaultBreaks } from '../constants';
 
 describe('media-queries', () => {
   describe('eitherGetBreakVal', () => {
     it('return breakpoint value if breakpoint name is valid', () => {
       expect(
-        eitherGetBreakVal(DEFAULT_BREAKS, 'phone').fold(errorReport, (x) => x),
+        eitherGetBreakVal(defaultBreaks, 'phone').fold(errorReport, (x) => x),
       ).toEqual('576px');
     });
   });
@@ -18,7 +18,7 @@ describe('media-queries', () => {
   describe('eitherGetNextBreakName', () => {
     it('return next breakpoint name if breakpoint name is valid', () => {
       expect(
-        eitherGetNextBreakName(DEFAULT_BREAKS, 'phone').fold(
+        eitherGetNextBreakName(defaultBreaks, 'phone').fold(
           errorReport,
           (x) => x,
         ),
@@ -29,7 +29,7 @@ describe('media-queries', () => {
   describe('eitherGetNextBreakVal', () => {
     it('return next breakpoint value if breakpoint name is valid', () => {
       expect(
-        eitherGetNextBreakVal(DEFAULT_BREAKS, 'phone').fold(
+        eitherGetNextBreakVal(defaultBreaks, 'phone').fold(
           errorReport,
           (x) => x,
         ),
