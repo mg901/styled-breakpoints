d599111bd8f859e44e36fcf705542c610391700e
mg901 <mgreen097@gmail.com>
2019-01-18 00:06:18 +0300

    refactor: fix models

--- src/models.js
+++ src/models.js
@@ -13,3 +13,14 @@ export type ThemeWithBreakpoints = {
     breakpoints: BreakpointsMap,
   },
 };
+
+export const customBreaks: BreakpointsMap = {
+  xs: '576px',
+  sm: '768px',
+  md: '992px',
+  lg: '1200px',
+};
+
+export const customTheme: ThemeWithBreakpoints = {
+  theme: { breakpoints: customBreaks },
+};
