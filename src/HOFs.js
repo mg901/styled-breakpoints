2bdad302245d270f21675239aaf484d6871b2c69
Abu Shamsutdinov <yakotikadot@gmail.com>
2019-01-20 00:36:45 +0300

    fix(src): small fix

--- src/HOFs.js
+++ src/HOFs.js
@@ -6,5 +6,5 @@ export const withMinMedia = (minWidth: any): string =>
 export const withMaxMedia = (maxWidth: any): string =>
   `@media (max-width: ${maxWidth})`;
 
-export const widthMinAndMaxMedia = (minWidth: any, maxWidth: any): string =>
+export const withMinAndMaxMedia = (minWidth: any, maxWidth: any): string =>
   `@media (min-width: ${minWidth}) and (max-width: ${maxWidth})`;
