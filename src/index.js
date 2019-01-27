7e22b1212844e17a9d85f5e98ace462e877d90ac
mg901 <mgreen097@gmail.com>
2019-01-27 15:09:38 +0300

    refactor: raname fns params & add distructuring

--- src/index.js
+++ src/index.js
@@ -2,54 +2,54 @@
 
 import { calcMinWidthInPx, calcMaxWidthInPx } from './calculators';
 import { withMinMedia, withMaxMedia, withMinAndMaxMedia } from './HOFs';
-import type { ThemeWithOptionalBreakpoints } from './models';
+import type { ThemeWithCustomBreakpoints } from './models';
 
-type Up = (string) => (ThemeWithOptionalBreakpoints) => ?string;
-export const up: Up = (breakName) => (breaks) =>
+type Up = (string) => (ThemeWithCustomBreakpoints) => string;
+export const up: Up = (breakName) => ({ theme }) =>
   withMinMedia(
     calcMinWidthInPx(
       breakName,
-      breaks.theme,
-      breaks.theme.ERROR_PREFIX_FOR_STYLED_BREAKPOINTS,
+      theme,
+      theme.ERROR_PREFIX_FOR_STYLED_BREAKPOINTS,
     ),
   );
 
-type Down = (string) => (ThemeWithOptionalBreakpoints) => ?string;
-export const down: Down = (breakName) => (breaks) =>
+type Down = (string) => (ThemeWithCustomBreakpoints) => string;
+export const down: Down = (breakName) => ({ theme }) =>
   withMaxMedia(
     calcMaxWidthInPx(
       breakName,
-      breaks.theme,
-      breaks.theme.ERROR_PREFIX_FOR_STYLED_BREAKPOINTS,
+      theme,
+      theme.ERROR_PREFIX_FOR_STYLED_BREAKPOINTS,
     ),
   );
 
-type Between = (string, string) => (ThemeWithOptionalBreakpoints) => ?string;
-export const between: Between = (minBreak, maxBreak) => (breaks) =>
+type Between = (string, string) => (ThemeWithCustomBreakpoints) => string;
+export const between: Between = (minBreak, maxBreak) => ({ theme }) =>
   withMinAndMaxMedia(
     calcMinWidthInPx(
       minBreak,
-      breaks.theme,
-      breaks.theme.ERROR_PREFIX_FOR_STYLED_BREAKPOINTS,
+      theme,
+      theme.ERROR_PREFIX_FOR_STYLED_BREAKPOINTS,
     ),
     calcMaxWidthInPx(
       maxBreak,
-      breaks.theme,
-      breaks.theme.ERROR_PREFIX_FOR_STYLED_BREAKPOINTS,
+      theme,
+      theme.ERROR_PREFIX_FOR_STYLED_BREAKPOINTS,
     ),
   );
 
-type Only = (string) => (ThemeWithOptionalBreakpoints) => ?string;
-export const only: Only = (breakName) => (breaks) =>
+type Only = (string) => (ThemeWithCustomBreakpoints) => string;
+export const only: Only = (breakName) => ({ theme }) =>
   withMinAndMaxMedia(
     calcMinWidthInPx(
       breakName,
-      breaks.theme,
-      breaks.theme.ERROR_PREFIX_FOR_STYLED_BREAKPOINTS,
+      theme,
+      theme.ERROR_PREFIX_FOR_STYLED_BREAKPOINTS,
     ),
     calcMaxWidthInPx(
       breakName,
-      breaks.theme,
-      breaks.theme.ERROR_PREFIX_FOR_STYLED_BREAKPOINTS,
+      theme,
+      theme.ERROR_PREFIX_FOR_STYLED_BREAKPOINTS,
     ),
   );
