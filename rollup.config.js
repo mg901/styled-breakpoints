54ce675f8767dc424c96933c1e7c676aa21e1568
mg901 <mgreen097@gmail.com>
2019-09-16 00:39:38 +0300

    build(rollup): remove babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,7 @@
 import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
 import { uglify } from 'rollup-plugin-uglify';
-import babel from 'rollup-plugin-babel';
-import { terser } from 'rollup-plugin-terser';
-import { main, module, dependencies, peerDependencies } from './package.json';
+import { main, dependencies, peerDependencies } from './package.json';
 
 export default [
   {
@@ -17,19 +15,6 @@ export default [
       ...Object.keys(dependencies || {}),
       ...Object.keys(peerDependencies || {}),
     ],
-    plugins: [babel(), uglify(), resolve(), commonjs()],
-  },
-  {
-    input: './src/index.js',
-    output: {
-      file: module,
-      format: 'es',
-      indent: false,
-    },
-    external: [
-      ...Object.keys(dependencies || {}),
-      ...Object.keys(peerDependencies || {}),
-    ],
-    plugins: [resolve(), babel(), terser(), commonjs()],
+    plugins: [resolve(), commonjs(), uglify()],
   },
 ];
