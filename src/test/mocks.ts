f963867f41acd8d9ce1bbf936a22ee43919b4eea
mg901 <mgreen097@gmail.com>
2019-11-12 17:02:49 +0300

    chore: add mocks

---
+++ src/test/mocks.ts
@@ -0,0 +1,33 @@
+export const CONFIG_SYMBOL = '@typographist/styled';
+
+export const PROPS_WITH_CUSTOM_THEME = {
+  theme: {
+    breakpoints: {
+      sm: '576px',
+      md: '768px',
+      lg: '992px',
+      xl: '1200px',
+    },
+  },
+};
+
+export const PROPS_WITH_EMPTY_THEME = {
+  theme: {},
+};
+
+export const PROPS_WITH_TYPOGRAPHIST_THEME = {
+  theme: {
+    [CONFIG_SYMBOL]: {
+      mediaQueries: {
+        tablet: '768px',
+        desktop: '992px',
+        lgDesktop: '1200px',
+      },
+    },
+  },
+};
+
+export const BREAKPOINTS = PROPS_WITH_CUSTOM_THEME.theme.breakpoints;
+export const CUSTOM_THEME = PROPS_WITH_CUSTOM_THEME.theme;
+export const EMPTY_THEME = PROPS_WITH_EMPTY_THEME.theme;
+export const TYPOGRAPHIST_THEME = PROPS_WITH_TYPOGRAPHIST_THEME.theme;
