a4c612c335a521b286eaa66f94d0c49837ac1d1c
mg901 <mgreen097@gmail.com>
2020-11-12 18:01:57 +0300

    feat: add support for emotion

--- react/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { up, down, between, only } from '../core';
-
-type Breakpoint = typeof up | typeof down | typeof between | typeof only;
-
-export declare function useBreakpoint(breakpoint: Function): boolean;
