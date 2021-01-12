bf0d62dc9057021e791de4905b4bfb05f6b67d9b
Greg McKelvey <mckelveygreg@gmail.com>
2021-01-11 22:41:07 -0800

    add exception for MediaQueryList event listener for Safari < 14 (#881)

--- react-styled/index.js
+++ react-styled/index.js
@@ -22,6 +22,18 @@ const useBreakpoint = (breakpoint) => {
     // Ensure the correct value is set in state as soon as possible
     setIsBreak(mq.matches);
 
+    // Safari < 14 can't use addEventListener on a MediaQueryList
+    // https://developer.mozilla.org/en-US/docs/Web/API/MediaQueryList#Browser_compatibility
+    if (!mq.addEventListener) {
+      // Update the state whenever the media query match state changes
+      mq.addListener(handleChange);
+
+      // Clean up on unmount and if the query changes
+      return function cleanup() {
+        mq.removeListener(handleChange);
+      };
+    }
+
     // Update the state whenever the media query match state changes
     mq.addEventListener('change', handleChange);
 
