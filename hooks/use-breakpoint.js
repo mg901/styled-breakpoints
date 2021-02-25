c58e5050ac209011da5b84a3591a4955482ae2e6
Maxim <mgreen097@gmail.com>
2021-02-26 01:31:56 +0300

    fix(use-brekpoint): useBreakpoint works without theme (#911)

--- hooks/use-breakpoint.js
+++ hooks/use-breakpoint.js
@@ -1,25 +1,6 @@
-const {
-  useState,
-  useEffect,
-  useCallback,
-  createContext,
-  useContext,
-} = require('react');
-const { makeStyledBreakpoints } = require('../core');
-const { isEmpty } = require('../core/library');
+const { useState, useEffect } = require('react');
 
-const { getBreakpointsFromTheme } = makeStyledBreakpoints();
-
-const createUseBreakpoint = ({ useTheme }) => (breakpoint) => {
-  const defaultTheme = {
-    breakpoints: getBreakpointsFromTheme(),
-  };
-
-  const isEmptyTheme = isEmpty(useTheme());
-  const theme = isEmptyTheme
-    ? useContext(createContext(defaultTheme))
-    : useTheme();
-
+exports.createUseBreakpoint = ({ theme }) => (breakpoint) => {
   // Get the media query to match
   const query = breakpoint({
     theme,
@@ -28,14 +9,14 @@ const createUseBreakpoint = ({ useTheme }) => (breakpoint) => {
   // null means "indeterminate", eg if the `window` object isn't available
   const [isBreak, setIsBreak] = useState(null);
 
-  // Handler for the media query change event
-  const handleChange = useCallback((event) => {
-    setIsBreak(event.matches);
-  }, []);
-
   // Set up a media query matcher on mount and if the query changes
   useEffect(() => {
     const mq = window.matchMedia(query);
+
+    // Handler for the media query change event
+    const handleChange = (event) => {
+      setIsBreak(event.matches);
+    };
 
     // Ensure the correct value is set in state as soon as possible
     setIsBreak(mq.matches);
@@ -47,20 +28,20 @@ const createUseBreakpoint = ({ useTheme }) => (breakpoint) => {
       mq.addListener(handleChange);
 
       // Clean up on unmount and if the query changes
-      return mq.removeListener(handleChange);
+      return () => {
+        mq.removeListener(handleChange);
+      };
     }
 
     // Update the state whenever the media query match state changes
     mq.addEventListener('change', handleChange);
 
     // Clean up on unmount and if the query changes
-    return mq.removeEventListener('change', handleChange);
-  }, [query, handleChange]);
+    return () => {
+      mq.removeEventListener('change', handleChange);
+    };
+  }, [query]);
 
   // Return the current match state
   return isBreak;
-};
-
-module.exports = {
-  createUseBreakpoint,
 };
