89f29f1b4201d61da71b9d7b7454856a5f4500a3
Maxim <mgreen097@gmail.com>
2021-10-21 14:40:08 +0300

    chore(eslint): bump eslint from 7 to 8 (#1059)

--- hooks/use-breakpoint.js
+++ hooks/use-breakpoint.js
@@ -1,37 +1,39 @@
 const { useState, useEffect } = require('react');
 
-exports.createUseBreakpoint = ({ theme: useTheme }) => (breakpoint) => {
-  // Get the media query to match
-  const query = breakpoint({
-    theme: useTheme(),
-  }).replace(/^@media\s*/, '');
-  const [isBreak, setIsBreak] = useState(null);
+exports.createUseBreakpoint =
+  ({ theme: useTheme }) =>
+  (breakpoint) => {
+    // Get the media query to match
+    const query = breakpoint({
+      theme: useTheme(),
+    }).replace(/^@media\s*/, '');
+    const [isBreak, setIsBreak] = useState(null);
 
-  useEffect(() => {
-    const mq = window.matchMedia(query);
-    const handleChange = (event) => {
-      setIsBreak(event.matches);
-    };
-
-    setIsBreak(mq.matches);
-
-    // Safari < 14 can't use addEventListener on a MediaQueryList
-    // https://developer.mozilla.org/en-US/docs/Web/API/MediaQueryList#Browser_compatibility
-    if (!mq.addEventListener) {
-      // Update the state whenever the media query match state changes
-      mq.addListener(handleChange);
-
-      // Clean up on unmount and if the query changes
-      return () => {
-        mq.removeListener(handleChange);
+    useEffect(() => {
+      const mq = window.matchMedia(query);
+      const handleChange = (event) => {
+        setIsBreak(event.matches);
       };
-    }
-    mq.addEventListener('change', handleChange);
 
-    return () => {
-      mq.removeEventListener('change', handleChange);
-    };
-  }, [query]);
+      setIsBreak(mq.matches);
 
-  return isBreak;
-};
+      // Safari < 14 can't use addEventListener on a MediaQueryList
+      // https://developer.mozilla.org/en-US/docs/Web/API/MediaQueryList#Browser_compatibility
+      if (!mq.addEventListener) {
+        // Update the state whenever the media query match state changes
+        mq.addListener(handleChange);
+
+        // Clean up on unmount and if the query changes
+        return () => {
+          mq.removeListener(handleChange);
+        };
+      }
+      mq.addEventListener('change', handleChange);
+
+      return () => {
+        mq.removeEventListener('change', handleChange);
+      };
+    }, [query]);
+
+    return isBreak;
+  };
