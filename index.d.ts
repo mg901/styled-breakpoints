34c302386d51cc772c82f9421076a81f7a6690c6
Алешин Максим <mgreen097@gmail.com>
2022-01-19 05:23:08 +0300

    fix: add type declaration for `createTheme`

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,5 @@
 import { Orientation } from './styled-breakpoints';
+export { createTheme } from './styled-breakpoints';
 
 export declare function up(min: string, orientation?: Orientation): any;
 export declare function down(max: string, orientation?: Orientation): any;
