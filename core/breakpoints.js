ad6b6185d120ed9aa5d2ada0e0fcd4f9f9234d76
Maxim Alyoshin <mgreen097@gmail.com>
2023-02-05 17:12:24 +0300

    refactor(breakpoints): remove `memoize` function around `calcMaxWidth`

--- core/breakpoints.js
+++ core/breakpoints.js
@@ -53,9 +53,9 @@ exports.createBreakpoints = ({ breakpoints, errorPrefix } = defaultOptions) => {
   // See https://www.w3.org/TR/mediaqueries-4/#mq-min-max
   // Uses 0.02px rather than 0.01px to work around a current rounding bug in Safari.
   // See https://bugs.webkit.org/show_bug.cgi?id=178261
-  const calcMaxWidth = memoize((value) => {
+  const calcMaxWidth = (value) => {
     return `${parseFloat(value) - 0.02}px`;
-  });
+  };
 
   return {
     up: getValueByName,
