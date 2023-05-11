1c19e4f3b4e4c395c9ff37b36dd20ce3ebf02b3e
Maxim Alyoshin <mgreen097@gmail.com>
2023-05-11 14:31:30 +0400

    chore: move configs from `package.json` to the root

---
+++ .size-limit.js
@@ -0,0 +1,28 @@
+module.exports = [
+  {
+    name: 'breakpoints',
+    path: './breakpoints/create-breakpoints.prod.js',
+    import: '{ createBreakpoints }',
+    limit: '237 B',
+  },
+  {
+    name: 'breakpoints.dev',
+    path: './breakpoints/create-breakpoints.dev.js',
+    import: '{ createBreakpoints }',
+    limit: '782 B',
+  },
+  {
+    name: 'styled-breakpoints',
+    path: './styled-breakpoints/index.js',
+    import: '{ createStyledBreakpointsTheme }',
+    limit: '705 B',
+  },
+  {
+    name: 'styled-breakpoints + useMediaQuery',
+    import: {
+      './styled-breakpoints/index.js': '{ createStyledBreakpointsTheme }',
+      './use-media-query/index.js': '{ useMediaQuery }',
+    },
+    limit: '966 B',
+  },
+];
---
+++ lint-staged.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  '*.{j,t}s': ['eslint --fix', 'prettier --write'],
+};
