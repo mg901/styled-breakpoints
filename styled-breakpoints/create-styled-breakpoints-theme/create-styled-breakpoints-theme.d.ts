6fb2546c2e45d2125de8b8588b0fdfa8a34c5fda
Maxim Alyoshin <mgreen097@gmail.com>
2023-07-18 16:00:51 +0400

    fix(docs): add correct types for `theme`

--- styled-breakpoints/create-styled-breakpoints-theme/create-styled-breakpoints-theme.d.ts
+++ styled-breakpoints/create-styled-breakpoints-theme/create-styled-breakpoints-theme.d.ts
@@ -5,11 +5,6 @@ type Min = BreakpointKeys;
 
 type Max = Exclude<BreakpointKeys, 'xs'>;
 
-type Up = (min: Min, orientation?: Orientation) => string;
-type Down = (max: Max, orientation?: Orientation) => string;
-type Between = (min: Min, max: Max, orientation?: Orientation) => string;
-type Only = (key: BreakpointKeys, orientation?: Orientation) => string;
-
 interface Options {
   breakpoints?: Record<string, `${number}px`>;
   errorPrefix?: string;
