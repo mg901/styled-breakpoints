604ae2c8bb7ce6033d959a186deda60cb4065a43
Maxim Alyoshin <mgreen097@gmail.com>
2023-11-09 03:43:21 +0400

    test: make flat array

--- styled-breakpoints/with-orientation/with-orientation.prod.spec.js
+++ styled-breakpoints/with-orientation/with-orientation.prod.spec.js
@@ -3,10 +3,9 @@ const { withOrientation } = require('./with-orientation.prod');
 describe('withOrientation function', () => {
   describe('in production', () => {
     // Arrange
-    const testCases = [['landscape'], ['portrait']];
     const mediaQuery = '(min-width: 768px)';
 
-    it.each(testCases)(
+    it.each(['landscape', 'portrait'])(
       'should return a media query with orientation for %s',
       (orientation) => {
         // Act
