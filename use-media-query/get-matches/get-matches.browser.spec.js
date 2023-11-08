96c50daabf19d2508d62ee7d8c968aacc06a0d7b
Maxim Alyoshin <mgreen097@gmail.com>
2023-11-08 08:08:14 +0400

    test(use-media-query hook): reorganization

--- use-media-query/get-matches/get-matches.browser.spec.js
+++ use-media-query/get-matches/get-matches.browser.spec.js
@@ -5,20 +5,26 @@
 const { mockViewport } = require('jsdom-testing-mocks');
 const { getMatches } = require('./get-matches');
 
-describe('getMatch function in browser', () => {
-  it('should return true if screen width is equal to 768px', () => {
-    const viewport = mockViewport({ width: '768px' });
+describe('getMatch', () => {
+  describe('in browser', () => {
+    it('should return true if screen width is equal to 768px', () => {
+      // Arrange
+      const viewport = mockViewport({ width: '768px' });
 
-    expect(getMatches('(min-width: 768px)')).toBe(true);
+      // Act & Assert
+      expect(getMatches('(min-width: 768px)')).toBe(true);
 
-    viewport.cleanup();
-  });
+      viewport.cleanup();
+    });
 
-  it('should return false if screen width is less than 768px', () => {
-    const viewport = mockViewport({ width: '500px' });
+    it('should return false if screen width is less than 768px', () => {
+      // Arrange
+      const viewport = mockViewport({ width: '500px' });
 
-    expect(getMatches('(min-width: 768px)')).toBe(false);
+      // Act & Assert
+      expect(getMatches('(min-width: 768px)')).toBe(false);
 
-    viewport.cleanup();
+      viewport.cleanup();
+    });
   });
 });
