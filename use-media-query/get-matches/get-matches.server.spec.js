c42b3f70153195f30d53f7f60ce5620e0013c919
Maxim Alyoshin <mgreen097@gmail.com>
2023-11-08 08:08:14 +0400

    test(use-media-query hook): reorganization

--- use-media-query/get-matches/get-matches.server.spec.js
+++ use-media-query/get-matches/get-matches.server.spec.js
@@ -1,7 +1,10 @@
 const { getMatches } = require('./get-matches');
 
-describe('getMatches function on server', () => {
-  it('should return false as there is no window object on the server', () => {
-    expect(getMatches('(min-width: 768px)')).toBe(false);
+describe('getMatches', () => {
+  describe('on server', () => {
+    it('should return false as there is no window object on the server', () => {
+      // And & Assert
+      expect(getMatches('(min-width: 768px)')).toBe(false);
+    });
   });
 });
