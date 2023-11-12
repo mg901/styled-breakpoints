a4fbfb1259c0f719df92c5c1b20915630da9842b
mg901 <mgreen097@gmail.com>
2023-11-12 07:10:52 +0300

    test: add tests for `useMediaQuery` hook (#1670)

---
+++ use-media-query/index.browser.spec.js
@@ -0,0 +1,104 @@
+/**
+ * @jest-environment jsdom
+ */
+
+const { renderHook } = require('@testing-library/react');
+const { useMediaQuery } = require('./use-media-query');
+
+describe('useMediaQuery', () => {
+  const matches = {
+    '(min-width: 500px)': true,
+    '(min-width: 1000px)': false,
+  };
+
+  // Arrange
+  const setupMockMediaQuery = () => {
+    window.matchMedia = jest.fn((query) => ({
+      matches: matches[query] || false,
+      media: query,
+      onchange: null,
+      addListener: jest.fn(),
+      removeListener: jest.fn(),
+    }));
+  };
+
+  describe('In Safari Browser less than v14', () => {
+    // Arrange
+    beforeEach(() => {
+      setupMockMediaQuery();
+    });
+
+    test('returns true if media query matches', () => {
+      // Act
+      const { result } = renderHook(() => useMediaQuery('(min-width: 500px)'));
+
+      // Assert
+      expect(result.current).toBe(true);
+    });
+
+    test('returns false if media query does not match', () => {
+      // Act
+      const { result } = renderHook(() => useMediaQuery('(min-width: 1200px)'));
+
+      // Assert
+      expect(result.current).toBe(false);
+    });
+
+    test('updates when media query changes', () => {
+      // Arrange and Act
+      const { result, rerender } = renderHook(
+        ({ query }) => useMediaQuery(query),
+        { initialProps: { query: '(min-width: 500px)' } }
+      );
+
+      // Assert
+      expect(result.current).toBe(true);
+
+      // Act
+      rerender({ query: '(max-width: 800px)' });
+
+      // Assert
+      expect(result.current).toBe(false);
+    });
+  });
+
+  describe('In other browsers', () => {
+    // Arrange
+    beforeEach(() => {
+      setupMockMediaQuery();
+    });
+
+    test('returns true if media query matches', () => {
+      // Act
+      const { result } = renderHook(() => useMediaQuery('(min-width: 500px)'));
+
+      // Assert
+      expect(result.current).toBe(true);
+    });
+
+    test('returns false if media query does not match', () => {
+      // Act
+      const { result } = renderHook(() => useMediaQuery('(min-width: 1200px)'));
+
+      // Assert
+      expect(result.current).toBe(false);
+    });
+
+    test('updates when media query changes', () => {
+      // Arrange and Act
+      const { result, rerender } = renderHook(
+        ({ query }) => useMediaQuery(query),
+        { initialProps: { query: '(min-width: 500px)' } }
+      );
+
+      // Assert
+      expect(result.current).toBe(true);
+
+      // Act
+      rerender({ query: '(max-width: 800px)' });
+
+      // Assert
+      expect(result.current).toBe(false);
+    });
+  });
+});
