b460d7af2377f95c04513aa8e5023d6a3fc3850e
Maxim Alyoshin <mgreen097@gmail.com>
2024-08-27 15:30:57 +0300

    refactor: add ts-check

--- styled-breakpoints/with-orientation/index.prod.js
+++ styled-breakpoints/with-orientation/index.prod.js
@@ -1,2 +1,16 @@
+// @ts-check
+
+/**
+ * @typedef {Object} Options
+ * @property {string} mediaQuery - The media query string (e.g., '@media screen').
+ * @property {string} orientation - The orientation type (e.g., 'portrait' or 'landscape').
+ */
+
+/**
+ * Creates a media query string that includes orientation criteria.
+ *
+ * @param {Options} options - Options for generating the media query.
+ * @returns - The media query string with orientation criteria.
+ */
 exports.withOrientation = ({ mediaQuery, orientation }) =>
   `${mediaQuery} and (orientation: ${orientation})`;
