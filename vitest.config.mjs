e75b6299924d4a7db5c467a148aaa4784bfb8482
Maxim Alyoshin <mgreen097@gmail.com>
2026-06-22 22:33:16 +0300

    refactor: update test environment comment in vitest configuration

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig, configDefaults } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    environment: 'jsdom', // вместо node
+    environment: 'jsdom',
     coverage: {
       provider: 'istanbul',
       reporter: ['lcov'],
