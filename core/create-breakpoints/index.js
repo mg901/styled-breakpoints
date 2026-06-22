ea59c951d39ae60e1c5460bdf7418963fbdf94ba
Maxim Alyoshin <mgreen097@gmail.com>
2026-06-22 22:29:27 +0300

    refactor: remove old functional

--- core/create-breakpoints/index.js
+++
@@ -1,7 +0,0 @@
-/* istanbul ignore file */
-import { createBreakpoints as prod } from './index.prod.js';
-import { createBreakpoints as dev } from './index.dev.js';
-
-const createBreakpoints = process.env.NODE_ENV === 'production' ? prod : dev;
-
-export { createBreakpoints };
--- styled-breakpoints/with-orientation/index.js
+++
@@ -1,7 +0,0 @@
-/* istanbul ignore file */
-import { withOrientation as prod } from './index.prod.js';
-import { withOrientation as dev } from './index.dev.js';
-
-const withOrientation = process.env.NODE_ENV === 'production' ? prod : dev;
-
-export { withOrientation };
