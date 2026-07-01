a85cc295a36c1060f3872c313ece88ee4a80a7a4
Maxim Alyoshin <mgreen097@gmail.com>
2026-07-01 15:19:53 +0300

    test: 💍 enable globals in Vitest configuration

--- vitest.config.js
+++ vitest.config.js
@@ -2,6 +2,7 @@ import { defineConfig, configDefaults } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    globals: true,
     environment: 'jsdom',
     coverage: {
       reporter: ['lcov'],
